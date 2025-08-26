2a6ff6941fbff4c3c529828361ccd0327d1252c9
Kenrawr <20102188@usc.edu.ph>
2025-08-27 05:12:43 +0800

    Feature: initial collection page

--- WebApp/src/app/AdminUI/AdminDashBoard/Features/AdminCollection/page.tsx
+++ WebApp/src/app/AdminUI/AdminDashBoard/Features/AdminCollection/page.tsx
@@ -1,6 +1,12 @@
 
 import AdminCollection from "./AdminCollection"
+import AdminControls from "./AdminControls"
 
 export default function AdminCollectionPage() {
-    return <AdminCollection/>
+    return (
+<>
+<AdminControls/>
+<AdminCollection/>
+</>
+    ) 
   }
