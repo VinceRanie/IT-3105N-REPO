3b5a748a7b0df1c7c7bffe3117a616bcd5d57574
Kenrawr <20102188@usc.edu.ph>
2025-08-31 05:51:24 +0800

    Test for Deployment

---
+++ WebApp/src/app/UsersUI/UsersDashBoard/layout.tsx
@@ -0,0 +1,10 @@
+import UsersUI from "./page";
+
+export default function UserDashBoard(){
+    return (
+        <div>
+        <UsersUI/>
+        <p>test</p>
+        </div>
+)
+}
---
+++ WebApp/src/app/UsersUI/UsersDashBoard/page.tsx
@@ -0,0 +1,5 @@
+"use client"
+
+export default function UsersUI(){
+    return <p>I am a user of biocella</p>
+}
